4c97115975da590e5b4370316eaea31366367f31
wanxiaoT <wxk68391644@163.com>
2025-04-08 22:56:35 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -16,7 +16,7 @@
     </n-flex>
     <n-flex :size="4" class="text" align="center" vertical>
       <n-p depth="3">
-        <n-text depth="3" @click="jumpLink(linkData.github)">
+        <n-text depth="3" @click="jumpLink(metaData.project)">
           SiteStatus
         </n-text>
         Version {{ version }}
@@ -32,7 +32,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        <n-text depth="3" @click="jumpLink(metaData.author)"> IMSYY </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
